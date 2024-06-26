626cd04b5773e9c5effdbc50181b6ad29833cd6c
Levkov Gleb <levkov.gleb.dev@gmail.com>
2024-06-26 19:13:48 +0300

    fix: inline supabase options

--- src/supabase/index.ts
+++ src/supabase/index.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 import { Database } from './supabase.types'
 
 export const supabase = createClient<Database>(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
+  'http://192.168.193.157:8000',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
 )
